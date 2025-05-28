411e6e163161cf2120a36e64c1941eebae6f063a
Cabji <beardeddonkeyonline@gmail.com>
2025-05-29 04:46:32 +1000

    SteelCalcDV: successful connection and query of database table

--- SteelCalcDatabaseViewer.cpp
+++ SteelCalcDatabaseViewer.cpp
@@ -15,27 +15,26 @@ DatabaseViewer(parent)
 	// dev-note: we have delegated the db connection to the default constructor in this overloaded ctr.
 	// data acquisition
 	m_dbActiveTableName = tableName;
-    std::cout << "	m_dbFilename: " << m_dbFilename << std::endl << "	m_dbActiveTableName: " << m_dbActiveTableName << std::endl;
 
 	// override the filename and table name if provided
     if (dbFilename.empty()) 
 	{
+		m_dbFilename	= DEFAULT_DATABASE_FILENAME;
 		// no database filename to open so quit
-		std::cerr << "No database filename was passed to SteelCalcDatabaseViewer constructor so returning with no database connection." << std::endl;
-		return;
+		std::cerr << "	!! - No database filename was passed to SteelCalcDatabaseViewer constructor so using default value instead." << std::endl;
     }
 
-    m_dbFilename	= dbFilename;
     m_dbConnection	= std::make_unique<SQLite::Database>(m_dbFilename, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
 
     // Set up the query for the active table
     if (!m_dbConnection)
 	{
 		// database connection failed
-		std::cerr << "Connection to database file '" << m_dbFilename << "' failed." << std::endl;
+		std::cerr << "	Connection to database file '" << m_dbFilename << "' failed." << std::endl;
 		return;		
 	}
 
+	std::cout << "	Active Table Name: " << m_dbActiveTableName << std::endl;
 	m_dbQuery = std::make_unique<SQLite::Statement>(*m_dbConnection, "SELECT * FROM " + m_dbActiveTableName);
 
 	// Loop through the results and print each row
@@ -44,7 +43,7 @@ DatabaseViewer(parent)
 		int columnCount = m_dbQuery->getColumnCount();
 		for (int i = 0; i < columnCount; ++i)
 		{
-			std::cout << m_dbQuery->getColumn(i).getName() << ": " << m_dbQuery->getColumn(i).getText() << "  ";
+			std::cout << "	" << m_dbQuery->getColumn(i).getName() << ":			" << m_dbQuery->getColumn(i).getText() << std::endl;
 		}
 		std::cout << std::endl;
 	}    
